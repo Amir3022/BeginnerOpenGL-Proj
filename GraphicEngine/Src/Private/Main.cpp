edc219673f5b84111afadc461cb32018cc527580
Amir3022 <amir.tayie98@gmail.com>
2025-12-02 13:26:53 +0200

    Removed all added code from position and transformation homework

--- GraphicEngine/Src/Private/Main.cpp
+++ GraphicEngine/Src/Private/Main.cpp
@@ -66,10 +66,10 @@ int main()
         //Create a vertices array   (Vertex Location, Vertex Color, Texture Coordinate)
         float vertices[] =
         {
-           -0.7f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
-           0.7f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
-           -0.7f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
-           0.7f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f
+           -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+           0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+           -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+           0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f
         };
 
         unsigned int indices[] =
@@ -132,31 +132,11 @@ int main()
                 shader.SetFloat("mixAlpha", texturesMixAlpha);
 
                 //Create Transform matrix to transform the drawn image
-                //Change texture rotation over time
-                float angleOverTime = glfwGetTime();
-                glm::mat4 transform = glm::identity<glm::mat4>();
-                transform = glm::translate(transform, glm::vec3(0.2f, -0.2f, 0.0f));
-                transform = glm::scale(transform, glm::vec3(0.5f));
-                transform = glm::rotate(transform, (angleOverTime * 2 * glm::pi<float>()) / 0.5f, glm::vec3(0.0f, 0.0f, 1.0f));
-
-                shader.SetMat44("inTransform", transform);
 
                 //Bind the VAO
                 glBindVertexArray(VAO);
 
                 //Draw triangle using previous data
-                glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0);
-
-                glBindVertexArray(0);
-
-                //Set a new transform to scale the same texture over time using a sin function
-                transform = glm::identity<glm::mat4>();
-                transform = glm::translate(transform, glm::vec3(-0.5f, 0.5f, 0.0f));
-                transform = glm::scale(transform, 0.4f * glm::vec3(glm::abs(glm::sin((glfwGetTime() * 2 * glm::pi<float>()) / 0.5f))));
-                shader.SetMat44("inTransform", transform);
-
-                glBindVertexArray(VAO);
-
                 glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0);
 
                 glBindVertexArray(0);
