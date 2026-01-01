91b2239917151d8565f353adbbafe9aeb3f10102
Amir3022 <amir.tayie98@gmail.com>
2026-01-01 11:30:40 +0200

    Changed the Phong Shading model to be in the view space instead of Model space

--- GraphicEngine/Shaders/LightScene/VertexShader.glsl
+++ GraphicEngine/Shaders/LightScene/VertexShader.glsl
@@ -5,7 +5,9 @@ layout (location = 1) in vec3 aNormal;
 
 out vec3 outNormal;
 out vec3 FragPos;
+out vec3 outLightSourcePos;
 
+uniform vec3 lightSourcePos;
 uniform mat3 normalModelMatrix;
 uniform mat4 model;
 uniform mat4 view;
@@ -14,6 +16,7 @@ uniform mat4 projection;
 void main()
 {
 	outNormal = normalModelMatrix * aNormal;
-	FragPos = vec3(model * vec4(aPos, 1.0f));
+	outLightSourcePos = vec3(view * vec4(lightSourcePos, 1.0f));
+	FragPos = vec3(model * view * vec4(aPos, 1.0f));
 	gl_Position = projection * view * model * vec4(aPos , 1.0f);
 }
