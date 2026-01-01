e9ee121ebe5fa42499dc3ea608099996da158a8a
Amir3022 <amir.tayie98@gmail.com>
2026-01-01 11:42:46 +0200

    Used Gouraud shading model instead by making the light calculations in vertex shader instead of the fragment shader

--- GraphicEngine/Shaders/LightScene/FragmentShader.glsl
+++ GraphicEngine/Shaders/LightScene/FragmentShader.glsl
@@ -2,29 +2,9 @@
 
 out vec4 FragColor;
 
-in vec3 FragPos;
-in vec3 outNormal;
-
-uniform vec3 lightSourcePos;
-uniform vec3 cameraPos;
-uniform vec3 objectColor;
-uniform vec3 lightColor;
-uniform float ambient;
-uniform float specularStrength;
+in vec3 outColor;
 
 void main()
 {
-	//Calculating Diffuse
-	vec3 norm = normalize(outNormal);
-	vec3 lightDir = normalize(lightSourcePos - FragPos);
-	float diffuse = max(dot(norm, lightDir), 0.0f);
-
-	//Calculating Specular
-	vec3 viewDir = normalize(cameraPos - FragPos);
-	vec3 reflectedLightDir = normalize(reflect(-lightDir, norm));
-	float specular = specularStrength * pow(max(dot(reflectedLightDir, viewDir), 0.0f), 64); //64 is shininnes value
-
-	//Combining Ambient, Diffuse, Specular for complete Phong Lighting Model
-	vec3 combined = (ambient + diffuse + specular) * lightColor;
-	FragColor = vec4(combined * objectColor, 1.0f);
+	FragColor = vec4(outColor, 1.0f);
 }
--- GraphicEngine/Shaders/LightScene/VertexShader.glsl
+++ GraphicEngine/Shaders/LightScene/VertexShader.glsl
@@ -3,8 +3,14 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 
-out vec3 outNormal;
-out vec3 FragPos;
+out vec3 outColor;
+
+uniform vec3 lightSourcePos;
+uniform vec3 cameraPos;
+uniform vec3 objectColor;
+uniform vec3 lightColor;
+uniform float ambient;
+uniform float specularStrength;
 
 uniform mat3 normalModelMatrix;
 uniform mat4 model;
@@ -13,7 +19,22 @@ uniform mat4 projection;
 
 void main()
 {
-	outNormal = normalModelMatrix * aNormal;
-	FragPos = vec3(model * vec4(aPos, 1.0f));
+	vec3 Normal = normalModelMatrix * aNormal;
+	vec3 FragPos = vec3(model * vec4(aPos, 1.0f));
+
+	//Calculating Diffuse
+	vec3 norm = normalize(Normal);
+	vec3 lightDir = normalize(lightSourcePos - FragPos);
+	float diffuse = max(dot(norm, lightDir), 0.0f);
+
+	//Calculating Specular
+	vec3 viewDir = normalize(cameraPos - FragPos);
+	vec3 reflectedLightDir = normalize(reflect(-lightDir, norm));
+	float specular = specularStrength * pow(max(dot(reflectedLightDir, viewDir), 0.0f), 64); //64 is shininnes value
+
+	//Combining Ambient, Diffuse, Specular for complete Phong Lighting Model
+	vec3 combined = (ambient + diffuse + specular) * lightColor;
+	outColor = combined * objectColor;
+
 	gl_Position = projection * view * model * vec4(aPos , 1.0f);
 }
