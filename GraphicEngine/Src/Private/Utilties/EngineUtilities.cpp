2ffa343c0639ec4d1d85ab20f5393b78c77450a9
Amir3022 <amir.tayie98@gmail.com>
2026-01-25 00:14:31 +0200

    Added engine utilities to load cubemap texture from image paths

--- GraphicEngine/Src/Private/Utilties/EngineUtilities.cpp
+++ GraphicEngine/Src/Private/Utilties/EngineUtilities.cpp
@@ -60,3 +60,66 @@ unsigned int EngineUtilities::LoadImageIntoTexture(const char* imagePath, bool b
 
     return outTexture;
 }
+
+unsigned int EngineUtilities::LoadImagesIntoCubemap(const std::vector<std::string>& imagePaths)
+{
+    unsigned int outTexture = -1;
+    int width, height, nChannels;
+
+    stbi_set_flip_vertically_on_load(false);
+
+    //Generate Cubemap Texture
+    glGenTextures(1, &outTexture);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, outTexture);
+    //Set Texture Parameters
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    //Load each Image path into a texture side of the cubemap Texture
+    for (int i = 0; i < (int)imagePaths.size();i++)
+    {
+        std::string imagePath = imagePaths[i];
+        unsigned char* data = stbi_load(imagePath.c_str(), &width, &height, &nChannels, 0);
+        if (data)
+        {
+            //Get the Data format based on image number on channels
+            GLenum dataFormat = GL_RED;
+            switch (nChannels)
+            {
+            case 1:
+            {
+                dataFormat = GL_RED;
+                break;
+            }
+            case 3:
+            {
+                dataFormat = GL_RGB;
+                break;
+            }
+            case 4:
+            {
+                dataFormat = GL_RGBA;
+                break;
+            }
+            }
+            //Load image data to Texture object
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, dataFormat, GL_UNSIGNED_BYTE, data);
+        }
+        else
+        {
+            std::cout << "Failed to load texture with path: " << imagePath << std::endl;
+            //Unbind the generated texture
+            glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+            return -1;
+        }
+        //Free loaded Image
+        stbi_image_free(data);
+    }
+    //Unbind the generated texture
+    glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+    
+    return outTexture;
+}
--- GraphicEngine/Src/Public/Utilities/EngineUtilities.h
+++ GraphicEngine/Src/Public/Utilities/EngineUtilities.h
@@ -1,7 +1,11 @@
 #pragma once
+#include <vector>
+#include <string>
 
 class EngineUtilities
 {
 public:
 	static unsigned int LoadImageIntoTexture(const char* imagePath, bool bUseAlpha = false);
+
+	static unsigned int LoadImagesIntoCubemap(const std::vector<std::string>& imagePaths);
 };
