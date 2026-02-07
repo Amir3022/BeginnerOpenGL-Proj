0f85416f8f949530cd4b814845a7e132f645e40b
Amir3022 <amir.tayie98@gmail.com>
2026-02-07 16:56:09 +0200

    Modified the Geometry shader to draw a line strip from point vertex input

--- GraphicEngine/Shaders/GSSceneShaders/GeometryShader.glsl
+++ GraphicEngine/Shaders/GSSceneShaders/GeometryShader.glsl
@@ -1,11 +1,14 @@
 #version 330 core
 
 layout(points) in;
-layout(points, max_vertices = 1) out;
+layout(line_strip, max_vertices = 2) out;
 
 void main()
 {
-	gl_Position = gl_in[0].gl_Position;	//GS passthrough, the input point vertex is outputed as it is
+	gl_Position = gl_in[0].gl_Position + vec4(-0.1f, 0.0f, 0.0f, 0.0f);
+	EmitVertex();
+
+	gl_Position = gl_in[0].gl_Position + vec4(0.1f, 0.0f, 0.0f, 0.0f);
 	EmitVertex();
 
 	EndPrimitive();
