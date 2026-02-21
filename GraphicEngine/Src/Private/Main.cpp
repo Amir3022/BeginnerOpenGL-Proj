14d21bf51cf9c2eba1c4fb063338551cf944a653
Amir3022 <amir.tayie98@gmail.com>
2026-02-21 16:03:31 +0200

    Committed missing project configurations

--- GraphicEngine/Src/Private/Main.cpp
+++ GraphicEngine/Src/Private/Main.cpp
@@ -1,8 +1,8 @@
-#include "GameModules/ShadowGame.h"
+#include "GameModules/PointShadowGame.h"
 
 int main()
 {
-    std::unique_ptr<ShadowGame> game = std::make_unique<ShadowGame>(1280, 720);
+    std::unique_ptr<PointShadowGame> game = std::make_unique<PointShadowGame>(1280, 720);
     if (game)
     {
         return game->Process();
